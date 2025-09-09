31041cde504c2d2c27e329e0b1f0b5599868bd52
losky2987 <losky2987@gmail.com>
2025-09-09 16:58:06 +0200

    SpringBoot initializr

---
+++ src/main/java/losky2987/RestaurantOrderSystem/RestaurantOrderSystemApplication.java
@@ -0,0 +1,13 @@
+package losky2987.RestaurantOrderSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantOrderSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantOrderSystemApplication.class, args);
+	}
+
+}
